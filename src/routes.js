2622c3e752ccca42c289bede0a40fd3de25a3a55
itzfeltrin <pedrofeltrin45@yahoo.com>
2020-11-14 00:01:36 -0300

    fix / route

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,10 @@ const routes = express.Router();
 
 const TeamController = require("./app/controller/TeamController");
 
+routes.get("/", (req, res) => {
+    res.redirect("/team");
+ });
+
 routes.get("/team", TeamController.index);
 routes.post("/team", TeamController.store);
 routes.delete("/team", TeamController.deleteAll);
